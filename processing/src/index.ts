a3bf935e64c0f76ee8fb54a427e02aca34498fd8
arjunsingh14 <arjunsingh140601@gmail.com>
2023-07-21 15:35:32 -0400

    videos are processed locally

---
+++ processing/src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import ffmpeg from "fluent-ffmpeg";
+const app = express();
+
+app.use(express.json());
+
+app.post("/process-video", (req, res) => {
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+  if (!inputFilePath || !outputFilePath) {
+    res.status(400).send("Bad request : Missing file path.");
+  }
+  ffmpeg(inputFilePath)
+    .outputOption("-vf", "scale=-1:360")
+    .on("end", () => {
+      res.status(200).send("Processing finished succesfully");
+    })
+    .on("error", (err) => {
+      console.log(`An error occuredL ${err.message}`);
+      res.status(500).send(`Internal server error: ${err.message}`);
+    })
+    .save(outputFilePath);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
